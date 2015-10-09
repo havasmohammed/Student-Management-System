7f3ddb66c27abe6d267a99a5c8ad1539f28807aa
havasmohammed <havas@qburst.com>
2015-10-09 17:36:22 +0530

    modified forms

--- sms/forms.py
+++ sms/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Basic_detail, Course_detail, Login_page
+from .models import Basic_detail, Course_detail, Login_page, Academic_detail
 
 
 class Login_pageForm(forms.ModelForm):
@@ -20,3 +20,16 @@ class Course_detailForm(forms.ModelForm):
     class Meta:
         model = Course_detail
         fields = ['cnum', 'cname', 'stream', 'credits', 'year_of_registration']
+
+
+class Academic_detailForm(forms.ModelForm):
+    class Meta:
+        model = Academic_detail
+        fields = ['Attendence_percentage',
+                  # 'cname',
+                  'First_year_percentage_of_marks',
+                  'Second_year_percentage_of_marks',
+                  'Third_year_percentage_of_marks',
+                  'Fourth_year_percentage_of_marks',
+                  'Total_percentage_of_marks',
+                  'Class_awarded']
