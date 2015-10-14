a54e9395769639c7a38622bd65499c2b38888a02
havasmohammed <havas@qburst.com>
2015-10-14 14:00:59 +0530

    modified forms

--- sms/forms.py
+++ sms/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Basic_detail, Course_detail, Login_page, Academic_detail
+from .models import Basic_detail, Course_detail, Login_page, Academic_detail, Miscellaneous_detail
 
 
 class Login_pageForm(forms.ModelForm):
@@ -12,14 +12,16 @@ class Login_pageForm(forms.ModelForm):
 class Basic_detailForm(forms.ModelForm):
     class Meta:
         model = Basic_detail
-        fields = ['first_name', 'last_name', 'sid', 'gender', 'address', 'DOB',
-                  'father_name', 'mother_name', 'email']
+        fields = ['first_name', 'middle_name', 'last_name', 'sid', 'gender', 'address', 'DOB',
+                  'father_name', 'mother_name', 'email', 'blood_group', 'father_profession',
+                  'family_income', 'religion', 'caste', 'reservation_category',
+                  'emergency_contact_number']
 
 
 class Course_detailForm(forms.ModelForm):
     class Meta:
         model = Course_detail
-        fields = ['cnum', 'cname', 'stream', 'credits', 'year_of_registration']
+        fields = ['cname', 'stream','code', 'year_of_registration', 'duration', 'level']
 
 
 class Academic_detailForm(forms.ModelForm):
@@ -33,3 +35,10 @@ class Academic_detailForm(forms.ModelForm):
                   'Fourth_year_percentage_of_marks',
                   'Total_percentage_of_marks',
                   'Class_awarded']
+
+
+class Miscellaneous_detailForm(forms.ModelForm):
+    class Meta:
+        model = Miscellaneous_detail
+        fields = ['nss', 'ncc','sports', 'hobbies', 'personal_website', 'orkut_profile_url',
+                  'facebook_profile_url', 'linkedin_profile_url']
