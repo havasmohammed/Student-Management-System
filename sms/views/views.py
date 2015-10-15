4c1d730af0bd477c73ab3523a1d6bf85ac5d08f0
havasmohammed <havas@qburst.com>
2015-10-15 11:35:11 +0530

    Added new views

---
+++ sms/views/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+from .forms import Login_pageForm, Basic_detailForm, Course_detailForm
+
+
+def index(request):
+    return HttpResponse("Student Information System")
+
+
+def loginpage(request):
+    title = "LOGIN PAGE "
+    print request
+    form = Login_pageForm(request.POST)
+    context = {
+        "title": title,
+        "form": form
+    }
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+    return render(request, "loginpage.html", context)
+
+
+def basicdetail(request):
+    title = "BASIC DETAILS"
+    print request
+    form = Basic_detailForm(request.POST)
+    context = {
+        "title": title,
+        "form": form
+    }
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+    return render(request, "basicdetail.html", context)
+
+
+def coursedetail(request):
+    title = "COURSE DETAILS"
+    print request
+    form = Course_detailForm(request.POST)
+    context = {
+        "title": title,
+        "form": form
+    }
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+    return render(request, "coursedetail.html", context)
